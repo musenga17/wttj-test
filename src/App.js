16e723da73ebefd237c1a46a0f4be0f9e18d103b
musenga17 <musenga17@gmail.com>
2020-12-17 17:02:08 +0100

    Remove unused files + Begining of development

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@welcome-ui/box'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box
+        display="flex"
+        width={1}
+        height="500px"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="nude.100"
+      >
+       
+      </Box>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, WuiProvider } from '@welcome-ui/core';
+
+const options = {
+  defaultFontFamily: 'Helvetica',
+  headingFontFamily: 'Georgia',
+  colors: {
+    primary: {
+      500: '#FF0000'
+    },
+    secondary: {
+      500: '#00FF00'
+    }
+  }
+};
+
+const theme = createTheme(options);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <WuiProvider
+    theme={theme}
+    hasGlobalStyle
+    useReset
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </WuiProvider>,
   document.getElementById('root')
 );
 
