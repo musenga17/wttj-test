c6cbb16435d0bcd18f13ab497cd4179773bf1905
musenga17 <musenga17@gmail.com>
2020-12-22 15:21:37 +0100

    End of all filters

--- src/components/offers/Offers.js
+++ src/components/offers/Offers.js
@@ -11,23 +11,73 @@ import Job from '../jobs/Job';
 import Axios from 'axios';
 import "./Offers.scss";
 
+var listOfJobsDefault = [];
+var apiCalled = false;
+var listOfOfficesNames = [];
+var listOfDepartmentNames = [];
 
 const Offers = () => {
 
   const [listOfJobs, setListOfJobs] = useState([]);
   const [listOfContractTypes, setListOfContractTypes] = useState([]);
+  const [searchJobValue, setSearchJobValue] = useState("");
+  const [contractTypeValue, setContractTypeValue] = useState("");
+  const [groupByValue, setGroupByValue] = useState("office.name");
+  const [dateValue, setDateValue] = useState(null);
 
   useEffect(() => {
-    Axios.get("https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k")
-      .then((result) => {
-        setListOfJobs(result.data.jobs);
-        getContractTypes(result.data.jobs);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+    if (!apiCalled) {
+      const fetchData = async () => {
+        await Axios.get("https://www.welcomekit.co/api/v1/embed?organization_reference=Pg4eV6k")
+          .then((result) => {
+            listOfJobsDefault = result.data.jobs;
+            setListOfJobs(result.data.jobs);
+            getContractTypes(result.data.jobs);
+            getOfficeNames(result.data.jobs);
+            getDepartmentNames(result.data.jobs);
+            apiCalled = true;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+        filterSearchJob();
+      };
+      fetchData();
+    }
+    else {
+      filterSearchJob();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [searchJobValue, contractTypeValue, dateValue, groupByValue]);
+
+  /**
+   * This method gets the office names in an array 
+   * without making duplicates
+   * 
+   * @param {Array} listOfJobs The list in which we will do the research
+  */
+  const getOfficeNames = (listOfJobs) => {
+    for (const job of listOfJobs) {
+      if (!listOfOfficesNames.includes(job.office.name)) {
+        listOfOfficesNames.push(job.office.name);
+      }
+    }
+  }
+
+  /**
+   * This method gets the department names in an array 
+   * without making duplicates
+   * 
+   * @param {Array} listOfJobs The list in which we will do the research
+  */
+  const getDepartmentNames = (listOfJobs) => {
+    for (const job of listOfJobs) {
+      if (!listOfDepartmentNames.includes(job.department.name)) {
+        listOfDepartmentNames.push(job.department.name);
+      }
+    }
+  }
 
   /**
    * This method determines if the array already contains an object whose the property value 'label'
@@ -39,27 +89,120 @@ const Offers = () => {
   const containsObjectLabel = (list, searchedString) => {
     const condition = (element) => element.label === searchedString;
     return list.some(condition);
-  }
+  };
 
   /**
    * This method gets the types of contracts in an array 
    * without making duplicates
    * 
-   * @param {Array} list The list in which we will do the research
+   * @param {Array} listOfJobs The list in which we will do the research
   */
   const getContractTypes = (listOfJobs) => {
     var listOfContractTypesTMP = [];
-    var index = 0;
     for (const job of listOfJobs) {
       if (!containsObjectLabel(listOfContractTypesTMP, job.contract_type.en)) {
         listOfContractTypesTMP.push({
-          value: `contractType${index}`,
+          value: job.contract_type.en,
           label: job.contract_type.en
         });
-        index++;
       }
     }
     setListOfContractTypes(listOfContractTypesTMP);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchJobValue(e.target.value);
+  };
+
+  const handleContractTypeChange = (e) => {
+    setContractTypeValue(e);
+  };
+
+  const handleDateChange = (e) => {
+    setDateValue(e);
+  };
+
+  const handleGroupByChange = (e) => {
+    setGroupByValue(e);
+  };
+
+  /**
+   * This method applies the filter for the search of jobs
+  */
+  const filterSearchJob = () => {
+    var listOfJobsTMP = listOfJobsDefault.filter((job) => job.name.toLowerCase().includes(searchJobValue.toLowerCase()));
+    setListOfJobs(listOfJobsTMP);
+    filterContractType(listOfJobsTMP);
+  };
+
+  /**
+   * This method applies the filter for the selected type of contract
+   * 
+   * @param {Array} listfJobsFiltered The list potentially already filtered
+  */
+  const filterContractType = (listOfJobsFiltered) => {
+    var listOfJobsTMP = [];
+    if (contractTypeValue !== "" && contractTypeValue !== undefined) {
+      listOfJobsTMP = listOfJobsFiltered.filter((job) => job.contract_type.en === contractTypeValue);
+    }
+    else {
+      listOfJobsTMP = listOfJobsFiltered;
+    }
+    setListOfJobs(listOfJobsTMP);
+    filterDate(listOfJobsTMP);
+  }
+
+  /**
+   * This method applies the filter for the selected date
+   * 
+   * @param {Array} listfJobsFiltered The list potentially already filtered
+  */
+  const filterDate = (listOfJobsFiltered) => {
+    var listOfJobsTMP = [];
+    if (dateValue !== null && dateValue !== undefined) {
+      listOfJobsTMP = listOfJobsFiltered.filter((job) => new Date(job.published_at) > new Date(dateValue));
+    }
+    else {
+      listOfJobsTMP = listOfJobsFiltered;
+    }
+    setListOfJobs(listOfJobsTMP);
+    filterGroupBy(listOfJobsTMP);
+  }
+
+  /**
+   * This method applies the filter for the selected group
+   * 
+   * @param {Array} listfJobsFiltered The list potentially already filtered
+  */
+  const filterGroupBy = (listOfJobsFiltered) => {
+    var listOfJobsTMP = [];
+    var listOfGroups = [];
+    var group = [];
+
+    switch (groupByValue) {
+      case 'office.name':
+        for (const officeName of listOfOfficesNames) {
+          group = listOfJobsFiltered.filter((job) => job.office.name === officeName);
+          listOfGroups.push(group);
+          // We decompose the elements (jobs) of all the groups in a single array
+          listOfJobsTMP = [].concat(...listOfGroups);
+        }
+        break;
+
+      case 'department.name':
+        for (const departmentName of listOfDepartmentNames) {
+          group = listOfJobsFiltered.filter((job) => job.department.name === departmentName);
+          listOfGroups.push(group);
+          // We decompose the elements (jobs) of all the groups in a single array
+          listOfJobsTMP = [].concat(...listOfGroups);
+        }
+        break;
+
+      default:
+        listOfJobsTMP = listOfJobsFiltered;
+        break;
+    }
+    setListOfJobs(listOfJobsTMP);
   }
 
   return (
@@ -72,10 +215,6 @@ const Offers = () => {
       <Text variant="h3" textAlign="center" marginBottom="50px">{data.offers.title}</Text>
       <Form
         onSubmit={() => console.log("onSubmit")}
-        initialValues={{
-          date: Date.now(),
-          groupBy: "department.name"
-        }}
       >
         {() =>
           <Box
@@ -89,6 +228,8 @@ const Offers = () => {
               component={InputText}
               placeholder={data.offers.searchInput.placeholder}
               className="inputField"
+              onChange={(e) => handleSearchChange(e)}
+              value={searchJobValue}
             />
             <ConnectedField
               name="contractType"
@@ -97,12 +238,15 @@ const Offers = () => {
               options={listOfContractTypes}
               isClearable
               className="select inputField"
+              onChange={(e) => handleContractTypeChange(e)}
+              value={contractTypeValue}
             />
             <ConnectedField
               name="date"
               component={DatePicker}
               placeholder={data.offers.datePickerInput.placeholder}
               className="inputField"
+              onChange={handleDateChange}
             />
             <ConnectedField
               name="groupBy"
@@ -110,12 +254,14 @@ const Offers = () => {
               placeholder={data.offers.groupBySelect.placeholder}
               options={data.offers.groupBySelect.options}
               className="select inputField"
+              onChange={(e) => handleGroupByChange(e)}
+              value={groupByValue}
             />
           </Box>
         }
       </Form>
       {listOfJobs.map((job) => (
-        <Job key={`job-${job.id}`} jobInformations={job} />
+        <Job key={`job-${job.id}`} jobInformations={job} searchTerm={searchJobValue} />
       ))}
     </Box>
   );
