d18cfc3c9485af40bea45c4f4019bfae00da7b2e
musenga17 <musenga17@gmail.com>
2020-12-22 16:09:25 +0100

    Addition of the highlighting in the name of the job during the search + Addition of the department name and the date of publication in the job display

--- src/components/jobs/Job.js
+++ src/components/jobs/Job.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box } from '@welcome-ui/box';
 import { Text } from '@welcome-ui/text';
 import OfferDescription from '../offers/OfferDescription';
+import Mark from "mark.js";
 
 const Job = (props) => {
+
+  useEffect(() => {
+    highlightSearchTerm(props.searchTerm);
+  }, [props.searchTerm]);
+
+  const convertDate = (inputFormat) => {
+    const pad = (s) => {
+      return (s < 10) ? '0' + s : s;
+    }
+    var d = new Date(inputFormat)
+    return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/')
+  }
+
+  /**
+   * This method highlight the search term in the job name
+   * 
+   * @param {string} searchTerm The search term
+  */
+  const highlightSearchTerm = (searchTerm) => {
+    // Element that will be highlighted
+    var textMarked = new Mark(".jobName");
+    textMarked.unmark({
+      done: () => {
+        textMarked.mark(searchTerm);
+      }
+    });
+  }
+
   return (
     <Box
       display="flex"
@@ -15,8 +44,10 @@ const Job = (props) => {
       marginBottom="10px"
     >
       <Box>
-        <Text variant="subtitle1" marginBottom="2px">{props.jobInformations.name}</Text>
-        <Text variant="subtitle2" color="dark.200">{props.jobInformations.contract_type.en} - {props.jobInformations.office.name}</Text>
+        <Text variant="subtitle1" marginBottom="2px" className="jobName">{props.jobInformations.name}</Text>
+        <Text variant="subtitle2" color="dark.200" marginBottom="10px">{props.jobInformations.contract_type.en} - {props.jobInformations.office.name}</Text>
+        <Text variant="subtitle2" color="orange" marginBottom="10px">Department : {props.jobInformations.department.name}</Text>
+        <Text variant="subtitle2" color="green">Published on {convertDate(props.jobInformations.published_at)}</Text>
       </Box>
       <OfferDescription jobInformations={props.jobInformations} />
     </Box>
